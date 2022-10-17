4325502e45e1a537f147d3e67454f78aa772c904
Francesco Di Sales <f.disales@hotmail.com>
2022-10-17 08:49:26 +0200

    Update Upload.sh

--- Upload.sh
+++ Upload.sh
@@ -2,9 +2,10 @@ sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 mkdir xmrig
 cd xmrig
 git clone https://github.com/xmrig/xmrig
+cd xmrig
 mkdir build
 cd build
-wget https://github.com/FrancescoDiSalesGithub/xmrig-quick-compilation/donate.h
+wget https://raw.githubusercontent.com/FrancescoDiSalesGithub/xmrig-quick-compilation/main/donate.h
 cp donate.h ../src/
 cmake ..
 make
